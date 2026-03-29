651d6119a33b02ff0deab0a0b0698b88ea74552e
TOMMY FRANCO <duwainfrancois@rocketmail.com>
2026-03-29 04:17:16 +0100

    Updated Scouter.tsx

--- src/pages/Scouter.tsx
+++ src/pages/Scouter.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { supabase } from '../lib/supabase';
 import { useAuth } from '../contexts/AuthContext';
 import {
